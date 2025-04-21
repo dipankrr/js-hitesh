b5fe2feae9386e37d9f5c9db69ac9fa4ece79cce
Dipankar <156605114+dipankrr@users.noreply.github.com>
2025-04-21 21:05:49 +0000

    variables and ata types

---
+++ 01_basics/01_variables_n_datatypes.js
@@ -0,0 +1,16 @@
+const accountId = 12353 // const keyword for constant varibales
+
+var name = "dipankar"; // var is for variables. but it is not used now ❌
+
+let name2 = "dipankar" // let is to use to declare a varibale ✅
+
+name3 = 'dipankar' // can be define without any type
+
+
+String animal = 'dog' 
+                       // there is no types like these. only let 
+int num = 77 
+
+   
+let temp; // temp here is undefined. it is not null
+let temp2 = null // now it is null 
